e1c0f90fbc1886d6e34cd7db3dd507846aeff46b
Ajay o s <akhubsmaster@gmail.com>
2022-03-28 18:40:39 +0530

    array

---
+++ python/array.py
@@ -0,0 +1,11 @@
+number = ["1", "2", "3", "4", "5", "6","7", "8", "9", "10"]; # inputing datas to number 
+print (number[0]); # print the 0 value from array output : 1
+print (number[1]); # print the 1 value from array output : 2
+print (number[2]); # print the 2 value from array output : 3
+print (number[3]); # print the 3 value from array output : 4
+print (number[4]); # print the 4 value from array output : 5
+print (number[5]); # print the 5 value from array output : 6
+print (number[6]); # print the 6 value from array output : 7
+print (number[7]); # print the 7 value from array output : 8
+print (number[8]); # print the 8 value from array output : 9
+print (number[9]); # print the 9 value from array output : 10
