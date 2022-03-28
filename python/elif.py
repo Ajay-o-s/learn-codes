3afc1bed459ec5706ed8db90391e962a07736d75
Ajay o s <akhubsmaster@gmail.com>
2022-03-28 18:29:54 +0530

    elif statement code example
    elif  statement code example

---
+++ python/elif.py
@@ -0,0 +1,7 @@
+a = int(input("enter a number : "));
+if (a<=10):
+    print ("number ",a ," is less than 10");
+elif (a<=20):
+    print ("number ",a ," is less than 20");
+else:
+    print ("number ",a," is greater than 20");
