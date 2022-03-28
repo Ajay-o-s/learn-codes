dc24d3e7ae0369bf33faab39c0922544c8aaf793
Ajay o s <akhubsmaster@gmail.com>
2022-03-28 18:28:55 +0530

    if-else  statement code example
    if-else  statement code example

---
+++ python/if-else.py
@@ -0,0 +1,6 @@
+# for more about see if statement code
+a = int(input("enter a number : "));
+if (a<=10):
+    print ("number ",a ," is less than 10");
+else:
+    print ("number ",a," is greater than 10");
