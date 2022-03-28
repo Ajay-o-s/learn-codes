6bb8a1f3679f44dff1f186e5b65ab8fe9ec1bd9e
Ajay o s <akhubsmaster@gmail.com>
2022-03-28 18:20:37 +0530

    if statement code example
    if statement code example

---
+++ if.py
@@ -0,0 +1,3 @@
+a = int(input("enter a number : ")); # inputing a number
+if (a<=10): # check the number less 10
+    print ("number ",a ," is less than 10"); # if the number is less than 10 then print number is less than 10
